57ad83c40880ca44f695ad05feb6a01560bd34ba
bakai <bakai90@inbox.ru>
2022-12-17 20:24:13 +0600

    jwt_tocen

--- src/main/java/com/example/spring_rest_api_session_java7/security/config/GlobalSecurityConfig.java
+++ src/main/java/com/example/spring_rest_api_session_java7/security/config/GlobalSecurityConfig.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-/**
- * author: Ulansky
- */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true,
 securedEnabled = true,
